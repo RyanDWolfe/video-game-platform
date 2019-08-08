4b136023cec15c544e803a28dfdb9c60d507e302
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-08-08 06:48:02 -0700

    add model associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+  belongs_to :developer
+  has_many :players, through: :game_sessions
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,5 @@
 class Player < ApplicationRecord
   has_secure_password
+  has_many :game_sessions
+  has_many :games, through: :game_sessions
 end
