9a35196c3104dbbb79778fac49df1205e40e0e42
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-08-15 12:26:46 -0500

    CRUD for Developers and Players

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,19 +1,25 @@
 class DevelopersController < ApplicationController
+  before_action :set_developer, only: [:show, :edit, :update, :destroy]
 
   def show
-
+    @game = Game.new
   end
 
   def index
-
+    @developers = Developer.all
   end
 
   def new
-
+    @developer = Developer.new
   end
 
   def create
-
+    @developer = Developer.new(developer_params)
+    if @developer.save
+      redirect_to @developer
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,10 +27,26 @@ class DevelopersController < ApplicationController
   end
 
   def update
-
+    if @developer.update(developer_params)
+      redirect_to @developer
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @developer.destroy
+    redirect_to developers_path
   end
+
+private
+
+  def set_developer
+    @developer = Developer.find(params[:id])
+  end
+
+  def developer_params
+    params.require(:developer).permit(:name)
+  end
+
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,28 +1,42 @@
 class PlayersController < ApplicationController
   before_action :set_player, only: [:show, :edit, :update, :destroy]
 
-  def show
-
-  end
-
-  def new
-
-  end
-
-  def create
-
-  end
-
   def edit
 
   end
+  
+  def show
+    #@game_session = Game_Session.new
+  end
+
+  def index
+    @players = Player.all
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to @player
+    else
+      render :new
+    end
+  end
 
   def update
-
+    if @player.update(player_params)
+      redirect_to @player
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @player.destroy
+    redirect_to players_path
   end
 
   private
@@ -32,9 +46,7 @@ class PlayersController < ApplicationController
   end
 
   def player_params
-    params.require(:player).permit(
-      :name,
-      :password
-      )
+    params.require(:player).permit(:name, :password)
   end
+
 end
