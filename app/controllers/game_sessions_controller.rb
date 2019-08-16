a789b5fbae8bb26d5610fad0a9aacb7c6ad6059e
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-08-16 10:39:19 -0500

    basic setup for game sessions, need to work on sign in first

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,11 +1,34 @@
 class GameSessionsController < ApplicationController
 
-  def new
-
-  end
+  before_action :set_game_session, only: [:show]
 
   def show
 
   end
-  
+
+  def new #abstract to a set_up_game method
+    @game_session = GameSession.create(
+      :player_id => params[:player_id],
+      :game_id => params[:game_id]) #need to put these into params
+  end
+
+  def create
+    @game_session = GameSession.new(game_session_params)
+    if @game_session.save
+      redirect_to @game_session
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_game_session
+    @game_session = GameSession.find(params[:id])
+  end
+
+  def game_session_params
+    params.require(:game_session).permit(:score, :health)
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,20 @@
 class Game < ApplicationRecord
   belongs_to :developer
   has_many :players, through: :game_sessions
+
+  # def play_game
+  #   @game = Game.new
+  #   redirect_to new_game_sessions_path
+  # end
+
+  # def game_outcome
+  #   if score == win_parameter
+  #     @win = 1
+  #   elsif health == lose_parameter
+  #     @lose = 1
+  #   else
+  #     playing
+  #   end
+  # end
+
 end
