aeb9f26fea48ea136d7851736073493eb58f8c47
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-08-16 11:58:33 -0500

    added sign in and sign out mechanic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  #before_action :current_user
+  before_action :current_player
   #before_action :require_logged_in, except: [:new, :create, :home] # Modify...
 
   def logged_in?
-    !!current_user
+    !!current_player
   end
 
 private
+
   def require_logged_in
-    redirect_to root_path unless logged_in? # Determine path...
+    redirect_to root_path unless logged_in?
   end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  def current_player
+    @current_player ||= Player.find(session[:player_id]) if session[:player_id]
   end
-  helper_method :current_user
+
+  helper_method :current_player
+  
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
 
   def new
-
+    @player = Player.new
+    @players = Player.all
   end
 
   def create
-
+    @player = Player.find_by(name: params[:player][:name])
+    if @player && @player.authenticate(params[:player][:password])
+      session[:player_id] = @player.id
+      redirect_to games_path #do I need to pass info here?
+    else
+      redirect_to signin_path
+    end
   end
 
   def destroy
