3a4570494de5e4c9d8719312a225f1f20c6ecbfb
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-08-21 16:22:01 -0500

    refactor of game sessions controller

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,23 +1,20 @@
 class GameSessionsController < ApplicationController
 
-  before_action :set_game_session, only: [:show]
+  before_action :set_game_session, only: [:show, :new]
 
   def show
 
   end
 
-  def new #abstract to a set_up_game method
-    @game = Game.find_by(id: params[:game_id])
-    @player = Player.find_by(id: session[:player_id])
-    @game_session = GameSession.new
-    @game_session.player_id = @player.id
-    @game_session.game_id = @game.id
+  def new 
+
   end
 
   def create
     @game_session = GameSession.new(game_session_params)
     @game_session.player_id = params[:player_id]
     @game_session.game_id = params[:game_id]
+    #game_outcome(@game_session)
     if @game_session.save
       redirect_to game_game_session_path(@game_session.game_id, @game_session.id)
     else
@@ -28,11 +25,13 @@ class GameSessionsController < ApplicationController
   private
 
   def set_game_session
-    @game_session = GameSession.find(params[:id])
+    @game_session = GameSession.find_by(id: params[:id]) || GameSession.new
+    @game = Game.find_by(id: params[:game_id])
+    @player = Player.find_by(id: session[:player_id])
   end
 
   def game_session_params
-    params.require(:game_session).permit(:score, :health, :game_id, :player_id)
+    params.require(:game_session).permit(:score, :health)
   end
 
 end
--- app/models/game_session.rb
+++ app/models/game_session.rb
@@ -1,4 +1,18 @@
 class GameSession < ApplicationRecord
   belongs_to :player
   belongs_to :game
+
+  def game_outcome(game_session)
+    @game = Game.find_by(id: @game_session.game_id)
+    if @game_session.health == @game.lose_parameter
+      @game_session.lose = 1
+      @game_outcome = "You Lost"
+    elsif @game_session.score == @game.win_parameter
+      @game_session.win = 1
+      @game_outcome = "You Won"
+    else
+      playing
+    end
+  end
+
 end
