bfe2f3311a2e91c62ce10930ec55639f3f9a1007
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-08-21 14:39:56 -0500

    ability to create game sessions

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -10,15 +10,18 @@ class GameSessionsController < ApplicationController
     @game = Game.find_by(id: params[:game_id])
     @player = Player.find_by(id: session[:player_id])
     @game_session = GameSession.new
+    @game_session.player_id = @player.id
+    @game_session.game_id = @game.id
   end
 
   def create
     @game_session = GameSession.new(game_session_params)
+    @game_session.player_id = params[:player_id]
+    @game_session.game_id = params[:game_id]
     if @game_session.save
-      #game_outcome(@game_session)
-      redirect_to @game_session
+      redirect_to game_game_session_path(@game_session.game_id, @game_session.id)
     else
-      render :new
+      render :new #this might need to be game path
     end
   end
 
@@ -29,7 +32,7 @@ class GameSessionsController < ApplicationController
   end
 
   def game_session_params
-    params.require(:game_session).permit(:score, :health)
+    params.require(:game_session).permit(:score, :health, :game_id, :player_id)
   end
 
 end
